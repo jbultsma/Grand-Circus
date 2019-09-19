599cd98efc0eb96aefbb0a0546d6cda9ffff4d20
jbultsma <joshuabultsma@gmail.com>
2019-09-19 18:29:12 -0400

    Added JS-Objects

---
+++ CircleTester/CircleTester/Circle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircleTester
+{
+    class Circle
+    {
+        public double radius;
+        public double GetRadius()
+        {
+
+
+            get
+            {
+                Console.Write("Please enter the radius: ");
+                string input = Console.ReadLine().Trim();
+                radius = double.Parse(input);
+            }
+
+            set
+            {
+                Console.WriteLine("Invalid Input, please try again!");
+                GetRadius();
+            }
+
+            return radius;
+        }
+        public Circle (double radius)
+        {
+            this.radius = Radius;
+        }
+
+        public double CalculateCircumfrence()
+        {
+            double circumfrence = (2 * Math.PI * Radius);
+
+            return circumfrence;
+        }
+
+    }
+}
---
+++ CircleTester/CircleTester/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace CircleTester
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Circle Tester!");
+            
+            GetRadius();
+            
+        }
+
+        static double GetRadius()
+        {
+            
+        }
+    }
+}
